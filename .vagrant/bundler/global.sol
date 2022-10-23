ddf1ae6b99be8aabdd0dde6c8c7b3c5088170cd2
noeltatah <noel.tatah@utrains.org>
2022-10-23 20:57:23 +0200

    added code

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"5b29b611799b05f93fc5a6a8d0b4b09cdd6d762586c7c8952fba21e7b75ea34d","vagrant_version":"2.3.0"}
